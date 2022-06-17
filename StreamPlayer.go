e12e89172aefe36e0841cb3779295c721a25d6dd
Peter B <31408676+pbelx@users.noreply.github.com>
2022-06-18 02:37:35 +0300

    Radio stream player
    Given url as argument play stream.

---
+++ StreamPlayer.go
@@ -0,0 +1,55 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/hajimehoshi/oto"
+
+	"github.com/hajimehoshi/go-mp3"
+)
+
+func run() error {
+
+	// musicfile := os.Args[1]
+
+	fmt.Println("Music server starting")
+	// f, err := os.Open(musicfile)
+	// if err != nil {
+	// 	return err
+	// }
+	// defer f.Close()
+
+	musicurl := os.Args[1]
+	resp, _ := http.Get(musicurl)
+
+	d, err := mp3.NewDecoder(resp.Body)
+	if err != nil {
+		return err
+	}
+
+	c, err := oto.NewContext(d.SampleRate(), 2, 2, 8192)
+	if err != nil {
+		return err
+	}
+	defer c.Close()
+
+	p := c.NewPlayer()
+	defer p.Close()
+
+	fmt.Printf("Length: %d[bytes]\n", d.Length())
+
+	if _, err := io.Copy(p, d); err != nil {
+		return err
+	}
+	return nil
+}
+
+func main() {
+	if err := run(); err != nil {
+		log.Fatal(err)
+	}
+}
